a4cb15d4580e55cbf1079273a0b1516fe9a05e6f
Ivan Netreba <73758348+Quattro9801@users.noreply.github.com>
2021-08-06 14:03:48 +0300

    Delete SpringBootApp/spring_BD_classic_model directory

--- SpringBootApp/spring_BD_classic_model/src/main/java/com/javatechie/jpa/repository/SheduleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.javatechie.jpa.repository;
-
-import com.javatechie.jpa.entity.SheduleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SheduleRepo extends JpaRepository<SheduleEntity,Long> {
-
-}
